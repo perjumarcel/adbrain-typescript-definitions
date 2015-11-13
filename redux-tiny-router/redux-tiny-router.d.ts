57bcc41d29b7cd21ef20abf5b9b46e1056113624
Slavomir Vojacek <slavomirvojacek@gmail.com>
2015-11-13 10:32:15 +0000

    update redux-tiny-router

--- redux-tiny-router/redux-tiny-router.d.ts
+++ redux-tiny-router/redux-tiny-router.d.ts
@@ -1,12 +1,18 @@
 declare module 'redux-tiny-router' {
-    function applyMiddleware(...props);
-
-    class tinyActions {
-        static navigateTo(url?:string, data?:any);
+    interface RouterState {
+        path?:string;
+        src?:string;
     }
+
+    function applyMiddleware(...props);
 
     class utils {
         static set(url?:string);
+
         static setRoutes(urls?:string[]);
+    }
+
+    class tinyActions {
+        static navigateTo(url?:string, data?:any);
     }
 }
